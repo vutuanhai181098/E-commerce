df9bd67af840bb297d63dc6744af2fe2e8ca7e3c
Hai <vutuanhai.18101998@gmail.com>
2023-12-05 17:46:10 +0700

    add

---
+++ main.js
@@ -0,0 +1,24 @@
+const showLoginForm = () => {
+    document.getElementById('login-form').style.display = 'block';
+    document.getElementById('register-form').style.display = 'none';
+    document.querySelector('#authModal .form-title').innerText = 'Đăng nhập';
+}
+const showRegisterForm = () => {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+    document.querySelector('#authModal .form-title').innerText = 'Đăng ký';
+}
+
+const showHidePassEl = document.querySelectorAll('.show-hide-password');
+showHidePassEl.forEach(e => {
+    e.addEventListener('click', () => {
+        const inputEl = e.previousElementSibling;
+        if(inputEl.type === "password"){
+            inputEl.setAttribute('type', 'text')
+            e.innerHTML = '<i class="fa-regular fa-eye"></i>'
+        } else {
+            inputEl.setAttribute('type', 'password')
+            e.innerHTML = '<i class="fa-regular fa-eye-slash"></i>'
+        }
+    })
+})
